01d4c143e5e3d5e3301b0e61c79f75818c411807
mshahzaib4 <shahzaibmalik0448@gmail.com>
2025-11-07 04:45:32 -0800

    data-ingestion complete

--- src/configuration/mongo_db_connection.py
+++ src/configuration/mongo_db_connection.py
@@ -1,34 +1,22 @@
-import os
-import sys
 import pymongo
 import certifi
-
-from src.exception import MyException
 from src.logger import logging
-from src.constants import DB_CONNECTION_URL, DB_COLLECTION_NAME
+from src.constants import DB_CONNECTION_URL, DB_NAME
 
-ca = certifi().where()
+ca = certifi.where()
 
 class MONGODBclient:
-
     client = None
 
-    def __init__(self, DATABASENAME:str)->None:
-        
+    def __init__(self, DATABASENAME: str = DB_NAME):
         try:
             if MONGODBclient.client is None:
-                db_url = os.getenv(DB_CONNECTION_URL)
-                if db_url is None:
-                    raise Exception(f"Environment variable '{DB_CONNECTION_URL}' is not set.")
-                
-                MONGODBclient.client = pymongo.MongoClient(db_url, tlsCAFile=ca)
-
+                MONGODBclient.client = pymongo.MongoClient(DB_CONNECTION_URL, tlsCAFile=ca)
+                logging.info("MongoDB client initialized successfully.")
+            
             self.client = MONGODBclient.client
-            self.database = self.client[DATABASENAME]
-            self.database_name = DATABASENAME
-            logging.info("Mongo database connection successfull!")
-
+            self.database = self.client[DATABASENAME]  # ✅ Add this line
+            logging.info(f"Connected to database: {DATABASENAME}")
 
         except Exception as e:
-            raise MyException(e, sys)
-              
+            raise e
